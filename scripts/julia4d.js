4a5b92309136f40e4da7c469b1c47c9f9dddc365
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-04-11 10:08:23 +0200

    refactor+doc

--- scripts/julia4d.js
+++ scripts/julia4d.js
@@ -1,57 +1,25 @@
-/*jshint evil: true, curly: false, asi:true, expr:true */
-/*global a:true, c:true, p:true,d:true, clearInterval: true, clearTimeout: true, setInterval: true, window:true */
+/*jshint evil:true curly:false asi:true expr:true */
+var running;
 exports.app = {
     underbar: true,
     type: 'canvas',
     start: function() {
-        c = this.elem;
-        a = c.getContext('2d');
+        running = true;
+        julia4d(this.elem);
 
-        c.width=796;
-        c.height=512;
-        /*
-        c.width=1000;
-        c.height=500;
-        c.style.width="95%";
-        c.style.height="95%";
-        */
-        var d,p, e, vs, k, pts, w0, wi0, w1, wi1, pts0, kcount, w, wi, j, Cx, Cy, v1, v2, x, y, i;
+    },
+    stop: function() {
+        running = false;
+    }
+};
+
+function julia4d(canvas) {
+    var pts;
+
+        var d,p, e, k, w0, wi0, w1, wi1, pts0, kcount, w, wi, j, Cx, Cy, v1, v2, x, y, i;
 
 
 
-        d = a.getImageData(0,0,c.width,c.height);
-        e = d.data;
-
-        vs = 1;
-        function blit() {
-            a.putImageData(d,0,0);
-        }
-
-        function setp(x,y,e) {
-            d.data[((y|0)*c.width+(x|0))*4+0] = 255&(e);
-            d.data[(y*c.width+x)*4+3] = 255;
-
-        }
-        function incp(x,y,e) {
-            if(0<x && x<c.width) {
-            d.data[((y|0)*c.width+(x|0))*4+0] += 50;
-        }
-        }
-
-        function fade() {
-            for(var i=e.length;i>0; i-=4) {
-                e[i-1]=255;
-                e[i-2]=255&(e[i-2]*0.999);
-                e[i-3]=255&(e[i-3]*0.999);
-                e[i-4]=255&(e[i-4]*0.999);
-            }
-        }
-        function clear() {
-            for(var i=0;i<e.length;i+=4) {
-                e[i]=0;
-                e[i+3]=255;
-            }
-        }
 
         k=0;
         pts = [];
@@ -68,30 +36,24 @@ exports.app = {
             wi0 += Math.random()*-0.5;
             w1 += Math.random()*-0.5;
             wi1 +=  Math.random()*-0.5;
-            //pts = pts0;
-            //pts0=[];
             k = 0;
-            setTimeout(drawpts,0);
-        //    d3();
+            if(running) {
+                setTimeout(drawpts,0);
+            }
         }
-                    function sign(x) {
-                        return x>0?1:-1;
-                    }
+        function sign(x) {
+            return x>0?1:-1;
+        }
         function drawpts() {
             for(;;){
             var z, zi, t, ti, r, s, Newz, Newzi;
             var random = Math.random;
             var sqrt = Math.sqrt;
-            //clear();
             w = k/kcount*w0 + (1-k/kcount) * w1;
             wi = k/kcount*wi0 + (1-k/kcount) * wi1;
-            // zn = z*z + w
-            // z = sqrt(zn - w);
                 z = random() * 2 - 1;
                 zi = random() * 2 - 1;
                 for(j=0;j<20;++j) {
-                    // sqrt(z+zi-w-wi) = sqrt((z-w)+(zi-wi)) =
-                    // r = sqrt(
                         Cx = w;
                         Cy = wi;
 
@@ -127,7 +89,6 @@ exports.app = {
 
                     pts[k] = ({x:z/8, y:zi/8, z:k/kcount/2-0.25});
                 }
-            //blit();
             ++k;
             if(k%300===0) {
             if(k < kcount) {
@@ -139,85 +100,49 @@ exports.app = {
             }
         }
         }
-        //drawpts();
-        //d3();
-        d3();
 
 
+    // ## Project the 3d point cloud onto the screen
+    (function() {
+        var ctx = canvas.getContext('2d');
+        var width = canvas.width = 512;
+        var height = canvas.height = 512;
+        var image = ctx.getImageData(0,0,width,height);
+        var pixels = image.data;
+        var baseAngle = 0;
         function transform(pt) {
-            v1 = 0.005*vs;
-            v2 = 0.01*vs;
-            var xa = pt.x * Math.cos(v1) - pt.y * Math.sin(v1);
-            var ya = pt.x * Math.sin(v1) + pt.y * Math.cos(v1);
-            var za = xa * Math.sin(v2) + pt.z * Math.cos(v2);
-            xa = xa * Math.cos(v2) - pt.z * Math.sin(v2);
-            return {x:xa, y:ya, z:za};
+            var a1 = 0.005*baseAngle;
+            var a2 = 0.01*baseAngle;
+            var x = pt.x * Math.cos(a1) - pt.y * Math.sin(a1);
+            var y = pt.x * Math.sin(a1) + pt.y * Math.cos(a1);
+            var z = x * Math.sin(a2) + pt.z * Math.cos(a2);
+            x = x * Math.cos(a2) - pt.z * Math.sin(a2);
+            return {x:x, y:y, z:z};
         }
-
-        function d3() {
+        function increasePixel(x,y) {
+            if(0<x && x<width) {
+                pixels[((y|0)*width+(x|0))*4+0] += 50;
+            }
+        }
+        function clear() {
+            for(var i=0;i<pixels.length;i+=4) {
+                pixels[i]=0;
+                pixels[i+3]=255;
+            }
+        }
+        function drawToScreen() {
                 clear();
                 for(var i=0;i<pts.length;++i) {
-                    p = pts[i];
+                    var p = pts[i];
                     p = transform(p);
-                    incp((p.x+0.5)*c.width, (p.y+0.5)*c.height);
-                    //console.log(p.x, p.y);
-                    //pts[i] = p;
+                    increasePixel((p.x+0.5)*width, (p.y+0.5)*height);
                 }
-                ++vs;
-                blit();
-                setTimeout(d3, 0);
-        }
-
-        x=y=100;
-        function f() {
-            for(var i=0;i<10000;++i){
-            x=Math.abs(x%c.width)+Math.random()-0.5;
-            y=Math.abs(y%c.height)+Math.random()-0.5;
-                d.data[((y|0)*c.width+(x|0))*4+0] += 1;
-            }
-            fade();
-            blit();
-            setTimeout(f, 100);
-        }
-        /*
-        t=0.1;
-        function f() {
-            t*=1.01
-            for(y=0;y<c.height;++y) {
-                for(x=0;x<c.width;++x) {
+                ++baseAngle;
+                ctx.putImageData(image,0,0);
+                if(running) {
+                    setTimeout(drawToScreen, 20);
                 }
-            }
-            blit();
-            setTimeout(f, 0);
         }
-        f()
-        */
-
-
-        for(i=0;i<d.data.length;++i) {
-        //    d.data[i] = 255&(Math.random()*256)
-        }
-        a.putImageData(d,0,0);
-        /*
-        c.style.width = "95%";
-        c.style.height = "95%";
-
-        for(y=0;y<c.height;++y) {
-            for(x=0;x<c.width;++x) {
-                d.data[(x+y*c.width)*4+1]=0;
-            }
-        }
-        */
-    },
-    stop: function() {
-        var i, t;
-        t = setTimeout(function(){}, 10000);
-        for(i = 0; i <= t; ++i) {
-            clearTimeout(i);
-        }
-        t = setInterval(function(){}, 10000);
-        for(i = 0; i <= t; ++i) {
-            clearInterval(i);
-        }
-    }
-};
+        drawToScreen();
+    })();
+}
--- scripts/plasma.js
+++ scripts/plasma.js
@@ -1,65 +1,86 @@
+// # Plasma
+//
+// App that draws a simple plasma cloud. Each color incremental.
+//
+// It uses the diamond-square algorithm, see http://en.wikipedia.org/wiki/Diamond-square_algorithm for details.
 exports.app = {
     underbar: true,
     type: 'canvas',
-start: function() {
-    var canvas = this.elem;
-    canvas.width = canvas.height = 512;
-    var ctx = canvas.getContext('2d');
-    
-    var img = ctx.createImageData(512,512);
-    var x, y;
-    
-    var col = 0;
+    start: function() {
+        var size = 128;
+        var canvas = this.elem;
+        canvas.width = canvas.height = size;
+        var ctx = canvas.getContext('2d');
+        var img = ctx.createImageData(size,size);
+        setTimeout(function() { drawPlasma(0); }, 0);
+        setTimeout(function() { drawPlasma(1); }, 0);
+        setTimeout(function() { drawPlasma(2); }, 0);
+        function drawPlasma(color) {
+            var p = diamondSquare(size);
+            var min = p.reduce(function(a,b) { return a<b?a:b; }, Infinity);
+            var max = p.reduce(function(a,b) { return a>b?a:b; }, -Infinity);
+            for(var i=0;i<img.data.length>>2; ++i) {
+                img.data[i*4+color] = (p[i]-min)*256/(max-min);
+                img.data[i*4+3] = 255;
+            }
+            ctx.putImageData(img,0,0);
+        }
+    }
+};
+
+
+// ## Diamond-square algorithm
+function diamondSquare(size, rnd) {
+    var step, x, y, sizeMask, result;
+    // Sanitise parameters
+    if(size & (size-1) !== 0) {
+        throw {error: 'size parameter must be a power of two'};
+    }
+    if(typeof rnd !== 'function') {
+        rnd = function(scale) {
+            return (Math.random()-0.5) * scale;
+        };
+    }
+
+    // The array which will contain the fractal.
+    // Initialisation of the first parameter is the base height of the fractal.
+    result = [0];
+
+    // Precalculated mask for module when out of boundaries.
+    sizeMask = size - 1;
+
+    // Shorthand function for getting a xy-element of the result array.
     function get(x,y) {
-        return img.data[((x&511)+(y&511)*512)*4+col];
+        return result[(x&sizeMask)+(y&sizeMask)*size];
     }
-    function put(x,y,c) {
-        img.data[((x&511)+(y&511)*512)*4+col] = c;
-    }
-    
-    var i = 0;
-    for(i=0;i<img.data.length>>2; ++i) {
-        img.data[i*4+3] = 255;
-    }
-    var step = 512;
-    function colrand() {
-        return (Math.random()-0.5) * step * 3;
-        //return (Math.random()-0.5) * step *1.4;
-        //return (Math.random()-0.5) * 80;
-    }
-    function plasma() {
-    while(step > 0) {
-        // diamond
+
+    for(step = size; step > 0; step >>= 1) {
         var prevStepMask = (step<<1) - 1;
-        for(x = 0; x < 512; x += step) {
-            for(y = 0; y < 512; y += step) {
+        // Square part of the algorithm
+        for(x = 0; x < size; x += step) {
+            for(y = 0; y < size; y += step) {
                 if((y&prevStepMask) && (x&prevStepMask)) {
-                    col = 0;
-                    put(x,y, (get(x-step, y-step) + get(x+step, y+step) + get(x+step,y-step) + get(x-step,y+step))/4 + colrand());
-                    col = 1;
-                    put(x,y, (get(x-step, y-step) + get(x+step, y+step) + get(x+step,y-step) + get(x-step,y+step))/4 + colrand());
-                    col = 2;
-                    put(x,y, (get(x-step, y-step) + get(x+step, y+step) + get(x+step,y-step) + get(x-step,y+step))/4 + colrand());
+                    result[x+y*size] = (
+                            get(x-step, y-step) +
+                            get(x+step, y+step) +
+                            get(x+step,y-step) +
+                            get(x-step,y+step))/4 + rnd(step);
                 }
             }
         }
-        for(x = 0; x < 512; x += step) {
-            for(y = 0; y < 512; y += step) {
-                if( (!(y&prevStepMask) && (x&prevStepMask)) || ((y&prevStepMask) && !(x&prevStepMask))
-                ) {
-                    col = 0;
-                    put(x,y, (get(x-step, y) + get(x+step, y) + get(x,y-step) + get(x,y+step))/4 + colrand());
-                    col = 1;
-                    put(x,y, (get(x-step, y) + get(x+step, y) + get(x,y-step) + get(x,y+step))/4 + colrand());
-                    col = 2;
-                    put(x,y, (get(x-step, y) + get(x+step, y) + get(x,y-step) + get(x,y+step))/4 + colrand());
+        // Diamond part of the algorithm
+        for(x = 0; x < size; x += step) {
+            for(y = 0; y < size; y += step) {
+                if( (!(y&prevStepMask) && (x&prevStepMask)) ||
+                    (!(x&prevStepMask) && (y&prevStepMask))) {
+                    result[x+y*size] = (
+                            get(x-step, y) +
+                            get(x+step, y) +
+                            get(x,y-step) +
+                            get(x,y+step))/4 + rnd(step);
                 }
             }
         }
-    
-        step >>= 1;
     }
-    }
-    plasma();
-    ctx.putImageData(img,0,0);
-}};
+    return result;
+}
