ee65823c342991deb9f90a9042d6a794f764f645
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-04-16 01:12:41 +0200

    no prettyprint in default run

--- yolan/run.sh
+++ yolan/run.sh
@@ -1,9 +1,8 @@
 rm test/yolan.js
 # compile the new compiler
-node yolan.js prettyprint yolan.yl yolan.new.yl
-node yolan.js compile yolan.new.yl yolan.new.js
+node yolan.js compile yolan.yl yolan.new.js
 # compile the new compiler with itself
-node yolan.new.js compile yolan.new.yl yolan.new.js
+node yolan.new.js compile yolan.yl yolan.new.js
 # compile testcases
 node yolan.new.js compile test/yolan.yl test/yolan.js
 # make sure the compiled code is as expected, or exit
@@ -11,7 +10,7 @@ diff -u test/yolan.js test/yolan.js.expected || exit 1
 # install the new compiler
 mv yolan.new.js yolan.js
 # make the new version of the compiler become the testcase
-cp yolan.new.yl test/yolan.yl
+cp yolan.yl test/yolan.yl
 cp yolan.js test/yolan.js.expected
 # run yolan with the newly compiled version
 node yolan.js $@
